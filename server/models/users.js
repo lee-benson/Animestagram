8919f2aced1bdeefe1a721982fc3a6e03293c475
Palealmond <trss95@outlook.com>
2023-03-17 10:28:02 -0400

    added initial media query

---
+++ server/models/users.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export const userSchema = new mongoose.Schema({
+  username: String,
+  hash: String,
+  handle: {
+    type: String,
+    unique: true,
+    lowercase: true,
+  },
+  posts: [{
+    ref: 'Post',
+    type: mongoose.Schema.Types.ObjectId,
+  }]
+})
+
+export default mongoose.model('User', userSchema)
