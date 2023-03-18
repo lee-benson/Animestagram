782384e26016beacfbaad4d245c65c581cf1e502
Benson Lee <BensonL1334@gmail.com>
2023-03-18 00:16:47 -0400

    Navbar looking good

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -14,12 +14,12 @@ export default function Navbar() {
       </h1>
       <nav>
         <div className="linkDiv">
-          <Link to="/" className="linkHome">Home</Link>
-          <Link to="/profile" className="linkProfile">Profile</Link>
-          <Link to="/sign-out" className="linkSignOut">Sign Out</Link>
+          <Link to="/" className="linkHome click-shadow-drop click-shadow-drop--blue">Home</Link>
+          <Link to="/profile" className="linkProfile click-shadow-drop click-shadow-drop--blue">Profile</Link>
+          <Link to="/sign-out" className="linkSignOut click-shadow-drop click-shadow-drop--blue">Sign Out</Link>
         </div>
-        <input type="text" className="searchBar" />
-        <button className="searchButton">Search</button>
+        <input type="text" placeholder="Type Anime Name" className="searchBar" />
+        <button className="searchButton click-shadow-drop click-shadow-drop--blue">Search</button>
       </nav>
     </header>
   )
