6860a3910a05f9a755c54c80308ff66879e898da
Benson Lee <BensonL1334@gmail.com>
2023-03-21 17:04:35 -0400

    commit for almond

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import './Navbar.css'
 export default function Navbar() {
   return (
     <header>
-      <h1>
+      <h1 className="navHeader">
         <div className="websiteTitleBlock">
           <p className="websiteTitleWords">
             Animestagram
@@ -14,9 +14,9 @@ export default function Navbar() {
       </h1>
       <nav>
         <div className="linkDiv">
-          <Link to="/" className="linkHome click-shadow-drop click-shadow-drop--blue">Home</Link>
-          <Link to="/profile" className="linkProfile click-shadow-drop click-shadow-drop--blue">Profile</Link>
-          <Link to="/sign-out" className="linkSignOut click-shadow-drop click-shadow-drop--blue">Sign Out</Link>
+          <Link to="/" className="linkHome navLink click-shadow-drop click-shadow-drop--blue">Home</Link>
+          <Link to="/profile" className="linkProfile navLink click-shadow-drop click-shadow-drop--blue">Profile</Link>
+          <Link to="/sign-out" className="linkSignOut navLink click-shadow-drop click-shadow-drop--blue">Sign Out</Link>
         </div>
         <input type="text" placeholder="Type Anime Name" className="searchBar" />
         <button className="searchButton click-shadow-drop click-shadow-drop--blue">Search</button>
