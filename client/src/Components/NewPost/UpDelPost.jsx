3a98bbab647c2b4acc841a34d6670efc1f825785
Palealmond <trss95@outlook.com>
2023-03-23 11:14:19 -0400

    Commented out update

--- client/src/Components/NewPost/UpDelPost.jsx
+++ client/src/Components/NewPost/UpDelPost.jsx
@@ -1,46 +1,46 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { DeletePost } from '../../api/posts.js';
 
 
 
 const UpDelForm = () => {
   const [title, setTitle] = useState('');
-  const [rating, setRating] = useState(0);
-  const [comment, setComment] = useState('');
+  // const [rating, setRating] = useState(0);
+  // const [comment, setComment] = useState('');
 
   const handleDeletePost = () => {
 
     DeletePost(title)
   };
 
-  const handleUpdatePost = () => {
+  // const handleUpdatePost = () => {
 
 
-    axios.put(`http://localhost:8080/api/post/${title}`, { rating, comment })
-      .then(response => {
-        console.log(response);
-        // handle successful update
-      })
-      .catch(error => {
-        console.log(error);
-        // handle error
-      });
-  };
+  //   axios.put(`http://localhost:8080/api/post/${title}`, { rating, comment })
+  //     .then(response => {
+  //       console.log(response);
+  //       // handle successful update
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //       // handle error
+  //     });
+  // };
 
   return (
     <div>
       <label htmlFor="title">Title:</label>
       <input type="text" id="title" value={title}
         onChange={e => setTitle(e.target.value)} />
-      <label htmlFor="rating">Rating:</label>
+      {/* <label htmlFor="rating">Rating:</label>
       <input type="number" id="rating" value={rating}
-        onChange={e => setRating(e.target.value)} />
-      <label htmlFor="comment">Comment:</label>
-      <textarea id="comment" value={comment}
-        onChange={e => setComment(e.target.value)}></textarea>
+        onChange={e => setRating(e.target.value)} /> */}
+      {/* <label htmlFor="comment">Comment:</label> */}
+      {/* <textarea id="comment" value={comment}
+        onChange={e => setComment(e.target.value)}></textarea> */}
       <button onClick={handleDeletePost}>Delete Post</button>
-      <button onClick={handleUpdatePost}>Update Post</button>
+      {/* <button onClick={handleUpdatePost}>Update Post</button> */}
     </div>
   );
 };
